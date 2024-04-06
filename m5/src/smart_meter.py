44673ab3a5f9f7691e25f660649b3381707e1c7d
soumaiabouh <soumaia.bouhouia@mail.mcgill.ca>
2024-04-06 12:05:44 -0400

    implemented the load_data method and modified the logic in generate key and id

--- m5/src/smart_meter.py
+++ m5/src/smart_meter.py
@@ -1,29 +1,41 @@
+import csv
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 class SmartMeter:
-    def __init__(self, dataCSV):
+    def __init__(self, pes_public_key, dataCSV):
+        self.pes_public_key = RSA.import_key(pes_public_key)
         self.data_dict = self.load_data(dataCSV)
-        self.generate_id()
-        self.generate_key()
+        self.id = self.generate_id()
+        self.aes_key = self.generate_key()
         
     def load_data(self, filename):
-        # load file, get data as dict, encrypt it using encrypt_data() and return it
-        pass
+        try:
+            with open(filename, mode='r', newline='') as file:
+                reader = csv.reader(file)
+                for i, row in enumerate(reader):
+                    if i >= 4320:  # Stop after 4,320 rows
+                        break
+                    timestamp, reading = row[0], row[1]
+                    self.encrypt_data_and_store(timestamp, reading)  # Encrypt each reading with its timestamp
+        except FileNotFoundError:
+            print(f"The file {filename} was not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
         
     def generate_key(self):
         # Generate a random 256-bit key
-        self._key = get_random_bytes(32)
+        return get_random_bytes(32)
     
     def generate_id(self):
         # generate id and return it
-        self.id = get_random_bytes(16)
+        return get_random_bytes(16)
         
     def encrypt_data(self, data):
         # helper function for load_data, takes the data and encrypts it using AES
-        cipher = AES.new(self._key, AES.MODE_GCM) # new object
+        cipher = AES.new(self.aes_key, AES.MODE_GCM) # new object
         ciphertext, tag = cipher.encrypt_and_digest(data) # encrypt plaintext 
         return ciphertext, tag, cipher.nonce # return cipher and nonce
         
