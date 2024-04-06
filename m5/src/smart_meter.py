c13e6784d8413c180ea68989722c5f3e2c5e8d63
soumaiabouh <soumaia.bouhouia@mail.mcgill.ca>
2024-04-06 12:18:44 -0400

    Made private most methods and modied the encrypt data method

--- m5/src/smart_meter.py
+++ m5/src/smart_meter.py
@@ -5,13 +5,14 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 class SmartMeter:
-    def __init__(self, pes_public_key, dataCSV):
+    def __init__(self, pes_public_key, filename):
         self.pes_public_key = RSA.import_key(pes_public_key)
-        self.data_dict = self.load_data(dataCSV)
-        self.id = self.generate_id()
-        self.aes_key = self.generate_key()
+        self.encrypted_data_list = []
+        self._load_data(filename)
+        self.id = self._generate_id()
+        self.aes_key = self._generate_key()
         
-    def load_data(self, filename):
+    def _load_data(self, filename):
         try:
             with open(filename, mode='r', newline='') as file:
                 reader = csv.reader(file)
@@ -19,34 +20,40 @@ class SmartMeter:
                     if i >= 4320:  # Stop after 4,320 rows
                         break
                     timestamp, reading = row[0], row[1]
-                    self.encrypt_data_and_store(timestamp, reading)  # Encrypt each reading with its timestamp
+                    self._encrypt_data_and_store(timestamp, reading)  # Encrypt each reading with its timestamp
         except FileNotFoundError:
             print(f"The file {filename} was not found.")
         except Exception as e:
             print(f"An error occurred: {e}")
         
-    def generate_key(self):
+    def _generate_key(self):
         # Generate a random 256-bit key
         return get_random_bytes(32)
     
-    def generate_id(self):
+    def _generate_id(self):
         # generate id and return it
         return get_random_bytes(16)
         
-    def encrypt_data(self, data):
-        # helper function for load_data, takes the data and encrypts it using AES
-        cipher = AES.new(self.aes_key, AES.MODE_GCM) # new object
-        ciphertext, tag = cipher.encrypt_and_digest(data) # encrypt plaintext 
-        return ciphertext, tag, cipher.nonce # return cipher and nonce
+    def _encrypt_data_and_store(self, timestamp, reading):        
+        cipher_aes = AES.new(self.aes_key, AES.MODE_GCM) # Important to generate a new cipher everytime for security reasons
+        encrypted_data, tag = cipher_aes.encrypt_and_digest(reading.encode('utf-8')) # Encrypt data using AES-GCM and converting string to bytes
+        nonce = cipher_aes.nonce # Nonce is needed for decryption in GCM mode
+        
+        self.encrypted_data_list.append({
+            'timestamp': timestamp,
+            'encrypted_data': encrypted_data,
+            'nonce': nonce,
+            'tag': tag
+        })
         
     def get_encrypted_data(self):
         # public function, return self.data_dict
-        return self.data_dict
+        return self.encrypted_data_list
             
-    def get_key(self, public_key):
-        # function that encrypts (RSA) smart meter key using a public key 
-        cipher_rsa = PKCS1_OAEP.new(public_key)
-        encrypted_key = cipher_rsa.encrypt(self._key)
+    def _encrypt_aes_key(self):
+        # function that encrypts using RSA the smart meter AES key using a public key 
+        cipher_rsa = PKCS1_OAEP.new(self.pes_public_key)
+        encrypted_key = cipher_rsa.encrypt(self.aes_key)
         return encrypted_key
 
 
