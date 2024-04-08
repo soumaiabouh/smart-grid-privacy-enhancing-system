462be2999d75afcb570b591c4865d592b782f5b2
soumaiabouh <soumaia.bouhouia@mail.mcgill.ca>
2024-04-08 19:00:10 -0400

    Simplified implementation of data_concentrator based on the smart meter and the pes implementations

--- m5/src/data_concentrator.py
+++ m5/src/data_concentrator.py
@@ -1,33 +1,24 @@
+from smart_meter import *
+from privacy_enhancing_system import *
+
 class DataConcentrator:
-    def __init__(self, PES):
+    def __init__(self, pes: PES):
         self.smList = []  # Initialize an empty list of smart meters
-        self.pes = PES  # Privacy enhancing system instance
-        self.data_dict = {}  # Dictionary to store data from each smart meter
+        self.pes = pes  # Privacy enhancing system instance
         self.time = 0  # Initialize time
         self.aggregated_data_dict = {}  # Dictionary to store aggregated data
-        self.decryption_keyAES = None
 
-    def add_smart_meter(self, sm):
+    def add_smart_meter(self, sm: SmartMeter):
         self.smList.append(sm)
 
-    def get_aggregated_data(self):
-        minutes = 10  # Number of minutes to aggregate data, hard-coded for nowZ
-        while self.time < minutes:
-            for sm in self.smList:
-                # Assuming sm.get_encrypted_data() returns encrypted data for a specific time
-                encrypted_data = sm.get_encrypted_data(self.time)
-                if sm.id not in self.data_dict:
-                    self.data_dict[sm.id] = []  # Initialize list for the smart meter if not present
-                # Append encrypted data for the current time to the data dictionary
-                self.data_dict[sm.id].append(encrypted_data)
-            self.time += 1
+    def get_aggregated_data(self, time_range = 60):
+        for sm in self.smList:
+            aggregated_data = self.pes.aggregate_and_encrypt(sm.get_encrypted_aes_key(), sm.get_encrypted_data())
+            
+            if sm.id not in self.aggregated_data_dict:
+                self.aggregated_data_dict[sm.id] = []  # Initialize list for the smart meter if not present
+            # Append encrypted data for the current time to the data dictionary
+            self.aggregated_data_dict[sm.id].append(aggregated_data)
 
-        # Aggregate data for each smart meter and store in aggregated_data_dict
-        for sm_id, encrypted_data_list in self.data_dict.items():
-            aggregated_data = self.pes.pallierEncrypt(self.decryption_keyAES, encrypted_data_list)
-            self.aggregated_data_dict[sm_id] = aggregated_data
-
-        # Clear data_dict for the next aggregation
-        self.data_dict.clear()
 
     # TODO: Implement send_to_database (send aggregated data dict)
