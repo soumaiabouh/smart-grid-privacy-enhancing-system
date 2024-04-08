9d0b76c78012f951ed5a83db5354ce07ec3513f4
soumaiabouh <soumaia.bouhouia@mail.mcgill.ca>
2024-04-08 18:19:33 -0400

    Added a shorter snapshot of the dataset to use for the demo

--- m5/src/test.py
+++ m5/src/test.py
@@ -8,7 +8,8 @@ if __name__=="__main__":
     pes_public_key = pes.get_public_key()
     
     sm1 = SmartMeter(pes_public_key, 
-                     "C:\\Users\\soums\\Desktop\\University\\W2024\\COMP555\\Project\\c555w24-t7\\m5\\src\\apart1.xlsx")
+                     "C:\\Users\\soums\\Desktop\\University\\W2024\\COMP555\\Project\\c555w24-t7\\m5\\src\\data\\demo\\apart1.xlsx")
 
     data = pes.aggregate_and_encrypt(sm1.get_encrypted_aes_key(), sm1.get_encrypted_data())
+    print(data)
     # Now we have the data. Need to send that to the MDMS, where it could possibly be decrypted. 
