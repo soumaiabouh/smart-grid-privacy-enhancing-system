fcbff9510558cff3d77bf7e6c75fa5358db3a4b4
soumaiabouh <soumaia.bouhouia@mail.mcgill.ca>
2024-04-08 10:45:01 -0400

    Created testing file

---
+++ m5/src/test.py
@@ -0,0 +1,11 @@
+from smart_meter import *
+from privacy_enhancing_system import *
+from data_concentrator import *
+
+if __name__=="__main__":
+    # Instantiate PES first to generate the key
+    pes = PES(1024)
+    pes_public_key = pes.get_public_key()
+    
+    sm1 = SmartMeter(pes_public_key, 
+                     "C:\\Users\\soums\\Desktop\\University\\W2024\\COMP555\\Project\\c555w24-t7\\m5\\src\\apart1.xlsx")
