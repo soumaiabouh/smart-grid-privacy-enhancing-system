21d6d55d216e7dee41b9a9f3c278c3b4ed85c1b9
soumaiabouh <soumaia.bouhouia@mail.mcgill.ca>
2024-04-09 14:30:01 -0400

    Now handling indices dynamically (keeps track of the index of the last reading for each smart meter loaded)

--- m5/src/data_concentrator.py
+++ m5/src/data_concentrator.py
@@ -5,20 +5,35 @@ class DataConcentrator:
     def __init__(self, pes: PES):
         self.smList = []  # Initialize an empty list of smart meters
         self.pes = pes  # Privacy enhancing system instance
-        self.time = 0  # Initialize time
         self.aggregated_data_dict = {}  # Dictionary to store aggregated data
+        self.last_processed_indices = {} # Maps smart meter IDs to last processed indices to avoid reaggregating everything
 
     def add_smart_meter(self, sm: SmartMeter):
         self.smList.append(sm)
 
-    def get_aggregated_data(self, start_index = 0):
+    def get_aggregated_data(self):
         for sm in self.smList:
-            aggregated_data = self.pes.aggregate_and_encrypt(sm.get_encrypted_aes_key(), sm.get_encrypted_data(), start_index)
+            self._aggregate_data_for_sm(sm)
             
-            if sm.id not in self.aggregated_data_dict:
-                self.aggregated_data_dict[sm.id] = []  # Initialize list for the smart meter if not present
-            # Append encrypted data for the current time to the data dictionary
-            self.aggregated_data_dict[sm.id].append(aggregated_data)
+        #DEBUG
+        # print("DATA CONCENTRATOR:")
+        # print(self.aggregated_data_dict)
+        # print(self.last_processed_indices)
+            
+    def _aggregate_data_for_sm(self, sm: SmartMeter):
+        id = sm.get_id()
+        encrypted_data = sm.get_encrypted_data()
+        
+        last_index = self.last_processed_indices.get(id, 0) 
+        aggregated_data = self.pes.aggregate_and_encrypt(sm.get_encrypted_aes_key(), encrypted_data, start_index=last_index)
+        
+        if aggregated_data:
+            new_last_index = last_index + len(encrypted_data[last_index:])
+            self.last_processed_indices[id] = new_last_index
+        
+            if sm.get_id() not in self.aggregated_data_dict:
+                self.aggregated_data_dict[id] = []
+            self.aggregated_data_dict[id].append(aggregated_data)
 
 
     # TODO: Implement send_to_database_manager (send aggregated data dict)
--- m5/src/main.py
+++ m5/src/main.py
@@ -9,15 +9,24 @@ if __name__=="__main__":
     
     filename1 = "C:\\Users\\soums\\Desktop\\University\\W2024\\COMP555\\Project\\c555w24-t7\\m5\\src\\data\\demo\\apart1.xlsx"
     
-    sm1 = SmartMeter(pes_public_key, filename1)
-    data = pes.aggregate_and_encrypt(sm1.get_encrypted_aes_key(), sm1.get_encrypted_data())
-    print(data)                 
-    print(len(sm1.encrypted_data_list))
+    sm1 = SmartMeter(pes_public_key, filename1, 10)
+    # data = pes.aggregate_and_encrypt(sm1.get_encrypted_aes_key(), sm1.get_encrypted_data())
+    # print(data)                 
+    # print(len(sm1.encrypted_data_list))
     
     sm1.generate_data(filename1, 10)
-    data2 = pes.aggregate_and_encrypt(sm1.get_encrypted_aes_key(), sm1.get_encrypted_data(), start_index=60)
-    print(data2)
-    print(len(sm1.encrypted_data_list))
+    # data2 = pes.aggregate_and_encrypt(sm1.get_encrypted_aes_key(), sm1.get_encrypted_data(), start_index=10)
+    # print(data2)
+    # print(len(sm1.encrypted_data_list))
+    
+    data_concentrator = DataConcentrator(pes)
+    data_concentrator.add_smart_meter(sm1)
+    data_concentrator.get_aggregated_data()
+    
+    sm1.generate_data(filename1, 10)
+    data_concentrator.get_aggregated_data()
+    
+    
     
     # print(data)
     # Now we have the data. Need to send that to the MDMS, where it could possibly be decrypted. 
