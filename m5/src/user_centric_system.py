04fd4073ee691ddff5cca050b36e7d41942a51d5
soumaiabouh <soumaia.bouhouia@mail.mcgill.ca>
2024-04-11 02:16:39 -0400

    Updated the command line interface for the UCS

--- m5/src/user_centric_system.py
+++ m5/src/user_centric_system.py
@@ -1,13 +1,14 @@
 from smart_meter import *
 from privacy_enhancing_system import *
-from mdms_manager import * 
+from mdms_manager import *
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
 import getpass
 import plotly.graph_objects as go
+import os
 
-#TODO: modify command line interface
 class UserCentricSystem:
     def __init__(self, sm: SmartMeter):
         self.sm = sm
@@ -16,101 +17,115 @@ class UserCentricSystem:
         self.salt = None
         self.key = None
         self.rsa_key_pair = None
-        
+
     def display(self):
-        # decrypts sm data from AES
         data_list = self.sm.get_encrypted_data()
-
-        # decrypt aes key for session 
-        # obtain encrypted key 
-        cipher_rsa = PKCS1_OAEP.new(self.rsa_key_pair)  # Creates a new RSA cipher object for decryption
-        aes_key = cipher_rsa.decrypt(self.encrypted_aes_key) 
-
-        # Decrypt each piece of reading and store it temporarily
+        cipher_rsa = PKCS1_OAEP.new(self.rsa_key_pair)
+        aes_key = cipher_rsa.decrypt(self.encrypted_aes_key)
         timestamps = []
-        power = [] 
-
+        power = []
         for data in data_list:
-            # Format of data: {timestamp, encrypted_data, tag, nonce}
             decrypted_data = self.decrypt(data, aes_key)
             timestamps.append(data['timestamp'])
             power.append(float(decrypted_data))
-        
-        # time series graph
-        fig = go.Figure() 
+        fig = go.Figure()
         fig.add_trace(go.Scatter(x=timestamps, y=power, mode='lines', name='Power [kW]'))
         fig.update_layout(title='Electricity Consumption', xaxis_title='Time', yaxis_title='Power [kW]')
         fig.show()
 
-        timestamps.clear()
-        power.clear()
-
     def decrypt(self, data, aes_key):
-        # helper function that decrypts data
         cipher_aes = AES.new(aes_key, AES.MODE_GCM, nonce=data['nonce'])
         return cipher_aes.decrypt_and_verify(data['encrypted_data'], data['tag'])
 
     def setCredentials(self, username: str, password: str):
-        # set username 
-        self.username = username 
-        # salt is randomly and uniquely generated for each user
-        # A (byte) string to use for better protection from dictionary attacks. 
-        # This value does not need to be kept secret, but it should be randomly chosen for each derivation. 
+        self.username = username
         self.salt = get_random_bytes(16)
-        # derive key from password
-        self.key = self.derive_key(password,self.salt)
-        # generate key pair 
+        self.key = self.derive_key(password, self.salt)
         self.rsa_key_pair = RSA.generate(2048)
-        # obtain encrypted key 
-        self.encrypted_aes_key = sm._encrypt_key(self.get_public())
-    
+        self.encrypted_aes_key = self.sm._encrypt_key(self.get_public())
+
     def get_public(self):
-        # fucntion returning public key 
         return self.rsa_key_pair.publickey()
-       
-    def login(self):
-        # "main" function
-        # If the username is NULL (aka this is the user's first login), then we prompt them for their username and password and call setCredentials().
-        # Ask user if they want to display data. If yes, prompt user to enter username and password to log in, then call display().
-        # Wait for user to log out/end the program.
 
-        if self.username is None:
-            print("New User")
-            username = input("Enter a username: ")
-            password = getpass.getpass("Enter a password: ")
+    def authenticate(self, username, password):
+        if username == self.username and self.derive_key(password, self.salt) == self.key:
+            return True
+        return False
+
+    def prompt_credentials(self, new_user=False):
+        username = input("Enter a username: ")
+        password = getpass.getpass("Enter a password: ")
+        if new_user:
             self.setCredentials(username, password)
-            print("Account successfully set up.")
-        
-        display_option = input("Display data? (YES/NO): ").upper()
-        if display_option == "YES":
-            username = input("Enter your username: ")
-            password = getpass.getpass("Enter your password: ")
-            if username == self.username:  # Checking if username matches
-                # Implement authentication logic with password
-                if self.derive_key(password, self.salt) == self.key: 
-                    self.display()
-                else:
-                    print("Incorrect password.")
+            return 'SETUP_COMPLETE'
+        elif not self.authenticate(username, password):
+            print("Incorrect username or password.")
+            return 'AUTH_FAILED'
+        return 'AUTH_SUCCESSFUL'
+
+    def user_dashboard(self):
+        print_header("User Dashboard")
+        while True:
+            display_option = input("Display data? (YES/NO), Logout (LOGOUT), or Exit (EXIT): ").upper()
+            if display_option == "YES":
+                self.display()
+            elif display_option == "LOGOUT":
+                return 'LOGOUT'
+            elif display_option == "EXIT":
+                return 'EXIT'
+            elif display_option == "NO":
+                print("\tMore options to come!")
+                continue
             else:
-                print("Invalid username.")
-        elif display_option == "NO":
-            pass
-        else:
-            print("Invalid option.")
+                print("Invalid option. Please choose YES, NO, LOGOUT, or EXIT.")
 
-    def derive_key(self, password: str, salt: bytes, key_length= 32):
-        # iteration count determines the computational complexity cost of key derivation process
-        derived = PBKDF2(password, salt, dkLen=key_length, count=1000000) # password based key derivation function
+    def login(self):
+        while True:
+            print_header("Login to Smart Meter Data Management")
+            if self.username is None:
+                result = self.prompt_credentials(new_user=True)
+                if result == 'SETUP_COMPLETE':
+                    print("\nAccount successfully set up.")
+                    input("\nPress Enter to continue...")
+                elif result == 'AUTH_FAILED':
+                    input("\nPress Enter to try again...")
+                    continue
+            else:
+                result = self.prompt_credentials()
+                if result == 'AUTH_FAILED':
+                    input("\nPress Enter to try again...")
+                    continue
+            action = self.user_dashboard()
+            if action == 'LOGOUT':
+                continue
+            elif action == 'EXIT':
+                print("\nExiting the program...")
+                break
+
+    def derive_key(self, password: str, salt: bytes, key_length=32):
+        derived = PBKDF2(password, salt, dkLen=key_length, count=1000000)
         return derived
-    
 
-# example usage
-mdms = MdmsManager(1024)
-mdms_key = mdms.get_public_key()
-pes = PES(mdms_key, 1024, 16)
-pes_public_key = pes.get_public_key()
+def clear_console():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
 
-filename = "data\\demo\\apart1.xlsx"
-sm = SmartMeter(pes_public_key, filename, 1440)
-user = UserCentricSystem(sm)
-user.login()
+def print_header(title):
+    clear_console()
+    term_width = os.get_terminal_size().columns
+    print(f"{'=' * term_width}")
+    print(f"{title.center(term_width)}")
+    print(f"{'=' *term_width}")
+
+if __name__ == "__main__":
+    mdms = MdmsManager(1024)
+    mdms_key = mdms.get_public_key()
+    pes = PES(mdms_key, 1024, 16)
+    pes_public_key = pes.get_public_key()
+
+    filename = "data\\demo\\apart1.xlsx"
+    sm = SmartMeter(pes_public_key, filename, 1440)
+    user = UserCentricSystem(sm)
+    user.login()
